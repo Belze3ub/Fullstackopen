340b45d5bc15536b28aed91872a497b8f1b222cb
Mikolaj Gawlik <mikolaj3313@gmail.com>
2023-12-09 22:43:32 +0100

    4.2 exercise

--- part4/index.js
+++ part4/index.js
@@ -1,39 +1,29 @@
-require('dotenv').config();
+const { MONGODB_URI, PORT } = require('./utils/config');
 const express = require('express');
 const app = express();
 const cors = require('cors');
+const { info, error } = require('./utils/middleware');
+const blogsRouter = require('./controllers/blogs');
 const mongoose = require('mongoose');
 
-const blogSchema = new mongoose.Schema({
-  title: String,
-  author: String,
-  url: String,
-  likes: Number,
-});
+mongoose.set('strictQuery', false);
 
-const Blog = mongoose.model('Blog', blogSchema);
+info('Connecting to', MONGODB_URI);
 
-const mongoUrl = process.env.MONGODB_URI;
-mongoose.connect(mongoUrl);
+mongoose
+  .connect(MONGODB_URI)
+  .then(() => {
+    info('Connected to MongoDB');
+  })
+  .catch((error) => {
+    error('Error connecting to MongoDB', error.message);
+  });
 
 app.use(cors());
 app.use(express.json());
 
-app.get('/api/blogs', (request, response) => {
-  Blog.find({}).then((blogs) => {
-    response.json(blogs);
-  });
-});
+app.use('/api/blogs', blogsRouter);
 
-app.post('/api/blogs', (request, response) => {
-  const blog = new Blog(request.body);
-
-  blog.save().then((result) => {
-    response.status(201).json(result);
-  });
-});
-
-const PORT = 3000;
 app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`);
+  info(`Server running on port ${PORT}`);
 });
