4941350aab75c2dd63a49961d75975ef8f71a9a2
Mikolaj Gawlik <mikolaj3313@gmail.com>
2023-12-13 20:53:57 +0100

    Added 'view' button to Blog component that displays more information about blog

--- part5/src/components/Blog.jsx
+++ part5/src/components/Blog.jsx
@@ -1,7 +1,34 @@
-const Blog = ({ blog }) => (
-  <div>
-    {blog.title} ~ {blog.author}
-  </div>  
-)
+import { useState } from 'react';
 
-export default Blog
+const Blog = ({ blog }) => {
+  const blogStyle = {
+    border: 'solid',
+    borderWidth: 1,
+    marginBlock: 5,
+    padding: 5,
+  };
+  const buttonStyle = { marginLeft: 5 };
+  const [showMore, setShowMore] = useState(false);
+
+  return (
+    <div style={blogStyle}>
+      <div>
+        {blog.title}
+        <button style={buttonStyle} onClick={() => setShowMore(!showMore)}>
+          {showMore ? 'close' : 'view'}
+        </button>
+      </div>
+      {showMore && (
+        <div>
+          <a href={blog.url}>{blog.url}</a>
+          <div>
+            likes {blog.likes} <button style={buttonStyle}>like</button>
+          </div>
+          <div>{blog.author}</div>
+        </div>
+      )}
+    </div>
+  );
+};
+
+export default Blog;
