7f85fbc7832115217aaa2aa69d72d1ccb1d9e684
Mikolaj Gawlik <mikolaj3313@gmail.com>
2023-12-20 18:28:27 +0100

    Exercises 6.20 - 6.22

---
+++ part6/query-anecdotes/src/App.jsx
@@ -0,0 +1,45 @@
+import AnecdoteForm from './components/AnecdoteForm';
+import Notification from './components/Notification';
+import { useQuery } from '@tanstack/react-query';
+import { getAnecdotes } from '../services/requests';
+import AnecdoteList from './components/AnecdoteList';
+
+const App = () => {
+  const result = useQuery({
+    queryKey: ['anecdotes'],
+    queryFn: getAnecdotes,
+  });
+
+  // console.log(JSON.parse(JSON.stringify(result)));
+  if (result.isLoading) return <p>Loading...</p>;
+  const anecdotes = result.data;
+  const error = result.error;
+
+
+  return (
+    <div>
+      {error ? (
+        <p>Anecdote service not available due to problems in server</p>
+      ) : (
+        <>
+          <h3>Anecdote app</h3>
+
+          <Notification />
+          <AnecdoteForm />
+          <AnecdoteList anecdotes={anecdotes} />
+          {/* {anecdotes.map((anecdote) => (
+            <div key={anecdote.id}>
+              <div>{anecdote.content}</div>
+              <div>
+                has {anecdote.votes}
+                <button onClick={() => handleVote(anecdote)}>vote</button>
+              </div>
+            </div>
+          ))} */}
+        </>
+      )}
+    </div>
+  );
+};
+
+export default App;
---
+++ part6/query-anecdotes/src/components/AnecdoteList.jsx
@@ -0,0 +1,35 @@
+import { useMutation, useQueryClient } from '@tanstack/react-query';
+import { updateAnecdote } from '../../services/requests';
+
+const AnecdoteList = ({ anecdotes }) => {
+  const queryClient = useQueryClient();
+  const updateAnecdoteMutation = useMutation({
+    mutationFn: updateAnecdote,
+    onSuccess: (anecdote) => {
+      const anecdotes = queryClient.getQueryData(['anecdotes']);
+      const newAnecdotes = anecdotes.map(oldAnecdote => oldAnecdote.id !== anecdote.id ? oldAnecdote : anecdote)
+      queryClient.setQueryData(['anecdotes'], newAnecdotes);
+    },
+  });
+  const handleVote = (anecdote) => {
+    updateAnecdoteMutation.mutate({
+      ...anecdote,
+      votes: anecdote.votes + 1,
+    });
+  };
+  return (
+    <div>
+      {anecdotes.map((anecdote) => (
+        <div key={anecdote.id}>
+          <div>{anecdote.content}</div>
+          <div>
+            has {anecdote.votes}
+            <button onClick={() => handleVote(anecdote)}>vote</button>
+          </div>
+        </div>
+      ))}
+    </div>
+  );
+};
+
+export default AnecdoteList;
